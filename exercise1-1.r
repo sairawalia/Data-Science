a9dd9054bebb28c6fc10f097f54eef956be97368
Saira <saira.del.ind@gmail.com>
2018-09-26 11:16:59 -0800

    Implemented multiple ways to access variables in R

--- exercise1-1.r
+++ exercise1-1.r
@@ -40,3 +40,18 @@ class(p)
 a <- murders$abb
 # Determine the class of a
 class(a)
+
+# We extract the population like this:
+p <- murders$population
+
+# This is how we do the same with the square brackets:
+o <- murders[["population"]]
+
+# We can confirm these two are the same
+identical(o, p)
+
+# Use square brackets to extract `abb` from `murders` and assign it to b
+b <- murders[["abb"]]
+a <- murders[["abb"]]
+# Check if `a` and `b` are identical
+identical(a, b)
